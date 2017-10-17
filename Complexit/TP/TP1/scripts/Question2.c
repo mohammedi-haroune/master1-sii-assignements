0d23e8f7af7afd425a84e628d3de21fc9cccd79b
MOHAMMEDI HAROUNE <mohammedi-bigmama@localhost.localdomain>
2017-10-17 22:01:23 +0200

    First attempt to calculate exectution time Problem : the excution time is always zero

--- Complexit/TP/TP1/scripts/Question2.c
+++ Complexit/TP/TP1/scripts/Question2.c
@@ -27,25 +27,44 @@
 
  #include<stdio.h>
  #include<stdlib.h>
+#include <time.h>
 
  int main()
  {
+	long int n = 1000000;
+	int test = 0;
+	while ( n < 10000000000000 ) {
 	long int i,N,S;
-
-	printf("Donner N = ");
-	scanf("%Ld",&N);
-
+	
+   	clock_t start_t, end_t, total_t;
+   	start_t = clock();
+//	printf("Donner N = ");
+//	scanf("%Ld",&N);
+	N = n;
 	S=0; i=1;
 
-	Do
+	do
 	{
 		S = S + i;
 		i = i + 1;
-	}while(i > N)
-
-	printf("La somme S = %Ld",S);
+	} while(i <= N);
+   	end_t = clock();
+	printf("La somme S = %Ld\n",S);
+   	total_t = (double) (end_t - start_t) / CLOCKS_PER_SEC;
+	printf("pour %ld Iteration le programme prends %lf\n", N, total_t);
+	if (test == 0) {
+	n = n*2;
+	test = 1;	
+	}
+	else {
+		if (test == 1) {
+		n = n*5;
+		test = 0;
+		}
+	}
+}
 	return 0;
- }
+}
 
  /***********************************************/
 
