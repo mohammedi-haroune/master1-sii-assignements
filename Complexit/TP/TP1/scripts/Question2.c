574fc2021e870ece7bafe3a7c628f36a7aa02c70
naila-houacine <32355075+naila-houacine@users.noreply.github.com>
2017-10-17 21:31:18 +0100

    Update Quest2 (time)

--- Complexit/TP/TP1/scripts/Question2.c
+++ Complexit/TP/TP1/scripts/Question2.c
@@ -25,22 +25,25 @@
 
 /**************************************************/
 
- #include<stdio.h>
- #include<stdlib.h>
+
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
  int main()
  {
-	long int n = 1000000;
-	int test = 0;
-	while ( n < 10000000000000 ) {
+
 	long int i,N,S;
-	
-   	clock_t start_t, end_t, total_t;
-   	start_t = clock();
-//	printf("Donner N = ");
-//	scanf("%Ld",&N);
-	N = n;
+
+   	clock_t start_t, end_t;
+   	double total_t;
+
+
+	printf("Donner N = ");
+	scanf("%Ld",&N);
+
+    start_t = clock();
+
 	S=0; i=1;
 
 	do
@@ -48,23 +51,16 @@
 		S = S + i;
 		i = i + 1;
 	} while(i <= N);
+
    	end_t = clock();
 	printf("La somme S = %Ld\n",S);
+
    	total_t = (double) (end_t - start_t) / CLOCKS_PER_SEC;
 	printf("pour %ld Iteration le programme prends %lf\n", N, total_t);
-	if (test == 0) {
-	n = n*2;
-	test = 1;	
-	}
-	else {
-		if (test == 1) {
-		n = n*5;
-		test = 0;
-		}
-	}
-}
+
 	return 0;
 }
+
 
  /***********************************************/
 
